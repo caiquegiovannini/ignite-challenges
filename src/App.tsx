15a388420aa346b5e6bc242d5506bdb760e7bd8c
Caique Giovannini <caique@qualyteam.com.br>
2021-08-02 22:46:40 -0300

    Create SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
 
 import { api } from './services/api';
 
@@ -60,8 +56,11 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
+      <SideBar 
+        genres={genres}
+        handleClickButton={handleClickButton}
+        selectedGenreId={selectedGenreId}
+      />
 
         <div className="buttons-container">
           {genres.map(genre => (
