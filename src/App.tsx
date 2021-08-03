a9711feebe97e8ae202156009499f08690b2dd73
Caique Giovannini <caique@qualyteam.com.br>
2021-08-02 22:47:08 -0300

    Create Content component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react';
 
 import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
 import './styles/global.scss';
-
-import './styles/sidebar.scss';
-import './styles/content.scss';
 
 interface GenreResponseProps {
   id: number;
@@ -62,33 +60,10 @@ export function App() {
         selectedGenreId={selectedGenreId}
       />
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content
+        selectedGenre={selectedGenre}
+        movies={movies}
+      />
     </div>
   )
 }
